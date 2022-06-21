e6219527de731cc7060df9e6709cdb21104c7123
Alex Canales <alexcanales766@gmail.com>
2022-06-20 21:05:46 -0400

    Init project

---
+++ app/main.py
@@ -0,0 +1,28 @@
+
+
+import os
+
+import discord
+from dotenv import load_dotenv
+
+
+if __name__ == "__main__":
+    
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    GUILD = os.getenv('DISCORD_GUILD')
+
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+    client.run(TOKEN)
