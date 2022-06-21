2ebcffafd7d612ad94a26436ca6cd7044f72796e
Alex Canales <alexcanales766@gmail.com>
2022-06-20 22:32:58 -0400

    Added network ping and license

--- app/utils/static_messages.py
+++ app/utils/static_messages.py
@@ -1,6 +1,7 @@
 
 def help_message(discord):
     embed=discord.Embed(title="Help", description="This is the help message!", color=0x0080ff)
+    embed.add_field(name="$ping", value="Shows network ping", inline=True)
     embed.add_field(name="$help", value="Shows this message", inline=True)
     embed.add_field(name="$author", value="Shows the author", inline=True)
     embed.set_footer(text="Let me know if you have any feedback! :)")
