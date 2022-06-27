c1f8453a8bd80d43fc205bc3151b479e95c13fa8
Alex Canales <alexcanales766@gmail.com>
2022-06-27 01:39:18 -0400

    Created dynamic date selection

--- app/utils/date_time.py
+++ app/utils/date_time.py
@@ -4,5 +4,14 @@ import datetime
 def get_date_in_yyyy_mm_dd_format():
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
+def get_date_yesterday_n_days(days_back):
+    return (datetime.datetime.now() - datetime.timedelta(days=days_back)).strftime("%Y-%m-%d")
+
 def get_date_yesterday_in_yyyy_mm_dd_format():
-    return (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    return get_date_yesterday_n_days(1)
+
+def get_last_three_dates_in_array_yyyy_mm_dd_format():
+    list_days = []
+    for i in range(1,4):
+        list_days.append(get_date_yesterday_n_days(i))
+    return list_days
--- app/utils/dynamic_messages.py
+++ app/utils/dynamic_messages.py
@@ -1,4 +1,14 @@
-from utils.date_time import get_date_yesterday_in_yyyy_mm_dd_format
+from utils.date_time import get_date_yesterday_in_yyyy_mm_dd_format, get_last_three_dates_in_array_yyyy_mm_dd_format
+
+def stock_trend_string_symbol(stock_difference):
+    stock_trend_string = ''
+    if stock_difference == 0:
+         stock_trend_string = "No change"
+    elif stock_difference < 0:
+         stock_trend_string = "📉"
+    elif stock_difference > 0:
+        stock_trend_string =  "📈"
+    return stock_trend_string
 
 def stock_message(discord, vantage_stock):
     date = get_date_yesterday_in_yyyy_mm_dd_format()
@@ -10,14 +20,8 @@ def stock_message(discord, vantage_stock):
     stock_open = stock_data['1. open']
     stock_close = stock_data['4. close']
     stock_difference = float(stock_close) - float(stock_open)
-    stock_trend_string = ""
-    if stock_difference == 0:
-         stock_trend_string = "No change"
-    elif stock_difference < 0:
-         stock_trend_string = "📉"
-    elif stock_difference > 0:
-        stock_trend_string =  "📈"
-        
+    stock_trend_string = stock_trend_string_symbol(stock_difference)
+    
     embed=discord.Embed(title=stock_symbol, description=stock_information, color=0x0080ff)
     embed.add_field(name="OPEN", value=stock_data['1. open'])
     embed.add_field(name="HIGH", value=stock_data["2. high"])
@@ -29,6 +33,26 @@ def stock_message(discord, vantage_stock):
     return embed
 
 def stock_list_message(discord, vantage_stock):
-    embed=discord.Embed(title="Stock List", description="Here is the list of stocks!", color=0x0080ff)
-    embed.add_field(name="Symbol", value=vantage_stock.meta_data['2. Symbol'])
+    date_array = get_last_three_dates_in_array_yyyy_mm_dd_format()
+    stock_information = vantage_stock.meta_data['1. Information']
+    stock_symbol = f"{vantage_stock.meta_data['2. Symbol']}"
+    
+    embed=discord.Embed(title=stock_symbol, description=stock_information, color=0x0080ff)
+    
+    for date in date_array:
+        print(date)
+        print( vantage_stock.time_series_daily)
+        stock_data = vantage_stock.time_series_daily[date]
+        stock_open = stock_data['1. open']
+        stock_close = stock_data['4. close']
+        stock_difference = float(stock_close) - float(stock_open)
+        stock_trend_string = stock_trend_string_symbol(stock_difference)
+        embed.add_field(name="OPEN", value=stock_data['1. open'])
+        embed.add_field(name="HIGH", value=stock_data["2. high"])
+        embed.add_field(name="LOW", value=stock_data["3. low"])
+        embed.add_field(name="CLOSE", value=stock_data["4. close"])
+        embed.add_field(name="VOL", value=stock_data["5. volume"])
+        embed.add_field(name="TREND", value=stock_trend_string)
+    
+
     return embed
